c6d019169fcd08a8907391f7d5ed611524e8feca
Navid Alvey <navidalvey@gmail.com>
2025-12-28 14:46:17 -0600

    Implemented holiday tree with random orniments

--- starter_code/HolidayArt.java
+++ starter_code/HolidayArt.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Holiday ASCII Art Generator
  * 
@@ -16,6 +18,9 @@ public class HolidayArt {
         // TODO: Parse command-line argument for tree height (default: 5)
         int height = 5;
 
+        if (args.length > 0) {
+            height = Integer.parseInt(args[0]);
+        }
         // TODO: Print the tree
         printTree(height);
     }
@@ -26,15 +31,34 @@ public class HolidayArt {
      * @param height Number of branch levels (not including star and trunk)
      */
     public static void printTree(int height) {
-        // TODO: Implement this method
-        //
-        // Steps:
-        // 1. Print the star on top (centered)
-        // 2. Loop through each level of branches
-        // - Calculate spaces needed for centering
-        // - Calculate stars needed (1, 3, 5, 7, ...)
-        // 3. Print the trunk (centered)
 
-        System.out.println("Implement me!");
+        Random rand = new Random(); // create once
+    
+        // 1. Print the star on top (centered)
+        System.out.print(" ".repeat(height));
+        System.out.println("*");
+
+        // 2. Loop through each level of branches
+        for (int level = 1; level <= height; level++) {
+    
+            int spaces = height - level;
+            int stars = 2 * level + 1;
+    
+            System.out.print(" ".repeat(spaces));
+    
+            // Rnadom orniments
+            for (int j = 0; j < stars; j++) {
+                if (rand.nextInt(4) == 0) {
+                    System.out.print(rand.nextBoolean() ? "o" : "O");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    
+        // 3. Print the trunk (centered)
+        System.out.print(" ".repeat(height - 1));
+        System.out.println("|||");
     }
 }
